d378c69e73536f6b96183429b76a99c0125a16cd
Plopmenz <plopmenz@gmail.com>
2026-05-07 19:42:58 +0200

    fix accessList property on Data

--- astro-app/src/lib/access.ts
+++ astro-app/src/lib/access.ts
@@ -34,51 +34,56 @@ export async function hasAccess({
   source: Source;
 }): Promise<string | undefined> {
   if (source.data[domain] !== undefined) {
-    const externalUser = users.find((user) =>
-      Object.keys(source.data[domain]).some((userReg) => {
-        // user matches this userReg
-        if (userReg.startsWith("regex:")) {
-          if (!new RegExp(userReg.replace("regex:", "")).test(user)) {
+    const externalUser = users.find((user) => {
+      let accessList = source.data[domain].accessList;
+      return (
+        accessList &&
+        accessList.users &&
+        Object.keys(accessList.users).some((userReg) => {
+          // user matches this userReg
+          if (userReg.startsWith("regex:")) {
+            if (!new RegExp(userReg.replace("regex:", "")).test(user)) {
+              return false;
+            }
+          } else {
+            if (userReg !== user) {
+              return false;
+            }
+          }
+
+          // source defines roles
+          let roles = accessList.roles;
+          if (!roles) {
             return false;
           }
-        } else {
-          if (userReg !== user) {
+
+          // source defines user and user has roles
+          let userRoles = accessList.users?.[userReg]?.roles;
+          if (!userRoles) {
             return false;
           }
-        }
 
-        // source defines roles
-        let roles = source.data[domain].roles;
-        if (!roles) {
-          return false;
-        }
-
-        // source defines user and user has roles
-        let userRoles = source.data[domain].users?.[userReg]?.roles;
-        if (!userRoles) {
-          return false;
-        }
-
-        return userRoles.some(
-          (role) =>
-            // role exists
-            Object.hasOwn(roles, role) &&
-            // role has access to path
-            new RegExp(roles[role].paths).test(path) &&
-            // there is no domainSpecific restrictions OR no restriction for this domain that rejects this user or path
-            (!source.restrictions?.domainSpecific ||
-              !source.restrictions.domainSpecific
-                .filter((restriction) =>
-                  new RegExp(restriction.domains).test(domain),
-                )
-                .some(
-                  (restriction) =>
-                    !new RegExp(restriction.users).test(user) ||
-                    !new RegExp(restriction.paths).test(path),
-                )),
-        );
-      }),
-    );
+          return userRoles.some(
+            (role) =>
+              // role exists
+              Object.hasOwn(roles, role) &&
+              // role has access to path
+              new RegExp(roles[role].paths).test(path) &&
+              // there is no domainSpecific restrictions OR no restriction for this domain that rejects this user or path
+              (!source.restrictions?.domainSpecific ||
+                !source.restrictions.domainSpecific
+                  .filter((restriction) =>
+                    new RegExp(restriction.domains).test(domain),
+                  )
+                  .some(
+                    (restriction) =>
+                      !new RegExp(restriction.users).test(user) ||
+                      !new RegExp(restriction.paths).test(path),
+                  )),
+          );
+        })
+      );
+    });
 
     if (externalUser !== undefined) {
       return externalUser;
--- astro-app/src/lib/source.ts
+++ astro-app/src/lib/source.ts
@@ -7,8 +7,10 @@ export interface Source {
 
 export interface Data {
   [domain: string]: {
-    users?: { [user: string]: { roles: string[] } };
-    roles?: { [role: string]: { paths: string } };
+    accessList?: {
+      users?: { [user: string]: { roles: string[] } };
+      roles?: { [role: string]: { paths: string } };
+    };
     config?: {
       ethereum?: {
         rpc?: string;
